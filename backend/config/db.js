107a28414a11a505c196ef1dc726ad218cd7c844
meehirlokhande <meehirlokhande@gmail.com>
2025-06-25 03:13:48 +0530

    Initial commit: User feedback app with React frontend and Node.js backend

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        });
+        console.log("Connected to Database Successfully.");
+    }catch(error){
+        console.error("Connection to Database failed");
+    }
+};
+
+module.exports = connectDB;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      
+      <div className='border-2 border-amber-300'>Hello World</div>
+      
+      
+    </>
+  )
+}
+
+export default App
