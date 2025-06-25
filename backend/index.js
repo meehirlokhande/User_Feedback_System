9e1785cee58c3f44a6e816c8a42c371408db645e
meehirlokhande <meehirlokhande@gmail.com>
2025-06-26 02:30:16 +0530

    Setup Express server and MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,28 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors =  require('cors');
+const connectDB = require('./config/db');
+dotenv.config();
+const app = express();
+
+//Database connection
+connectDB();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use('/auth', require('./routes/authRoutes'));
+app.use('/feedback',require('./routes/feedbackRoutes'));
+
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on ${PORT}`);
+})
+
+
